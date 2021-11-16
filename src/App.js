8d6b75cd2ae5a671bfbccd14486dba3e23c530cc
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-16 14:23:35 +0330

    Adding Pages and provider to Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { AuthProvider } from "./providers/AuthProvider";
+import Login from "./pages/Login/Login";
+import Chats from "./pages/Chats/Chats";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/chats" element={<Chats />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App;
