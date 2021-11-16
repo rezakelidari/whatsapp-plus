6797aab807af82b623f9ec98e6876f858e3ff996
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-16 14:18:35 +0330

    Making Title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./Title.module.css";
+
+function Title() {
+  return (
+    <div className={Styles.titleMain}>
+      Whatsapp<span>+</span>
+    </div>
+  );
+}
+
+export default Title;
